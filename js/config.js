5f7a68661c84fc7f9a4fbb31be5dee3449262863
mu-stf <harvykj30@gmail.com>
2026-01-24 09:17:01 +0300

    Add config.js for Vercel

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// نموذج ملف تكوين Supabase
+// انسخ هذا الملف إلى config.js وأدخل بياناتك
+
+const SUPABASE_CONFIG = {
+    url: 'https://hgffzlxbyvdkhtddgiti.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhnZmZ6bHhieXZka2h0ZGRnaXRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3NTM0ODgsImV4cCI6MjA4MjMyOTQ4OH0.M1v2_9KVRhI_vbEHpj2H3WZKTw1Iwn-e1t0db_5miFs'
+};
